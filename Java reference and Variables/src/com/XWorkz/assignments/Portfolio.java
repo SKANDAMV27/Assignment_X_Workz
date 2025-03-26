1b64877883ac360ce4bdb841753c77a01a21ec64
Skanda <skandagowda0@gmail.com>
2025-03-26 20:23:09 +0530

    Simple Code

---
+++ Java reference and Variables/src/com/XWorkz/assignments/Portfolio.java
@@ -0,0 +1,13 @@
+package com.XWorkz.assignments;
+
+public class Portfolio {
+String portfolioName="What";
+public void Portfolio(String portfolioName)
+{
+    this.portfolioName=portfolioName;
+}
+void displayPortfolio()
+{
+    System.out.println("Details OOf This Portfolio: ");
+}
+}
